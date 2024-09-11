e753ee6ff7962d45f7788061feacdbb66abfc270
OlayideAde <laide.ademeso@gmail.com>
2024-09-11 02:51:34 +0100

    setuphooks to get config variables per enviroment

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,29 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    
-    },
-    specPattern: 'cypress/e2e/tests/**/*.cy.js'
-  },
+      // get config file for test environment
+      const testEnv = config.env.testEnv || 'local'
+      const configFileName = `./configFiles/${testEnv}.config.json`
 
+      const settings = require(configFileName)
+
+      // set base url to config file baseUrl 
+      if (settings.baseUrl) {
+        config.baseUrl = settings.baseUrl
+      } else {
+        config.baseUrl = 'https://app.clickup.com/login'
+      }
+      // get env variables from config file
+      if(settings.env) {
+        config.env = {
+          ...config.env,
+          ...settings.env
+        }
+      }
+     
+      return config
+    },
+    specPattern: 'cypress/e2e/tests/**/*.cy.js',
+  },
   reporter: "mochawesome"
 });
